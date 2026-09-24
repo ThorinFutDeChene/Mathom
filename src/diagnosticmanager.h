aa561cfc02f3becc2d9f70392390f417f9121496
Thorin_01 <fabrice.pereyron@proton.me>
2026-09-24 19:56:16 +0200

    Add diagnostic session logger

---
+++ src/diagnosticmanager.h
@@ -0,0 +1,62 @@
+/**
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ */
+
+#ifndef DIAGNOSTICMANAGER_H
+#define DIAGNOSTICMANAGER_H
+
+#include <QObject>
+#include <QString>
+#include <QStringList>
+#include <QVariantMap>
+
+class QFile;
+class QTimer;
+class QWidget;
+
+class DiagnosticManager : public QObject
+{
+    Q_OBJECT
+
+public:
+    static DiagnosticManager &instance();
+
+    void startSession();
+    void closeSession();
+
+    void logEvent(const QString &event, const QVariantMap &details = {});
+    QString diagnosticsDirectory() const;
+    QString currentSessionId() const;
+    QStringList pendingReports() const;
+
+    void showPendingReportDialog(QWidget *parent = nullptr);
+
+protected:
+    bool eventFilter(QObject *watched, QEvent *event) override;
+
+private:
+    DiagnosticManager();
+    ~DiagnosticManager() override;
+
+    Q_DISABLE_COPY_MOVE(DiagnosticManager)
+
+    void inspectPreviousSessions();
+    void cleanupOldSessions(const QString &lastClosedSessionFile);
+    bool sessionClosedNormally(const QString &path) const;
+    QString createReportForSession(const QString &sessionPath);
+    void openReport(const QString &reportPath);
+    void prepareEmail(const QString &reportPath);
+    QVariantMap baseRecord(const QString &event) const;
+    void writeRecord(const QVariantMap &record);
+
+    QFile *m_sessionFile = nullptr;
+    QTimer *m_heartbeatTimer = nullptr;
+    QString m_sessionId;
+    QString m_sessionPath;
+    QStringList m_pendingReports;
+    qint64 m_startedAtMs = 0;
+    bool m_started = false;
+    bool m_closed = false;
+};
+
+#endif // DIAGNOSTICMANAGER_H

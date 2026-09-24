c578943346873e3a2d37fa70bd3ed2f9dc0a7844
Thorin_01 <fabrice.pereyron@proton.me>
2026-09-24 12:07:35 +0200

    Add integrated Debian update installer

--- src/updatechecker.h
+++ src/updatechecker.h
@@ -6,9 +6,13 @@
 
 #include <QObject>
 #include <QString>
+#include <QUrl>
 
 class QWidget;
+class QFile;
 class QNetworkAccessManager;
+class QProgressDialog;
+class QTemporaryDir;
 
 class UpdateChecker final : public QObject
 {
@@ -21,8 +25,33 @@ private:
     void start();
 
     QString installedVersion() const;
-    bool isVersionGreater(const QString &candidate, const QString &reference) const;
+
+    bool isVersionGreater(
+        const QString &candidate,
+        const QString &reference) const;
+
+    void downloadUpdate(
+        const QUrl &url,
+        const QString &fileName,
+        const QString &version,
+        const QString &expectedSha256);
+
+    bool verifySha256(
+        const QString &path,
+        const QString &expectedSha256) const;
+
+    bool validateDebPackage(
+        const QString &path,
+        const QString &expectedVersion) const;
+
+    void installUpdate(
+        const QString &path,
+        const QString &version);
 
     QWidget *m_parentWidget = nullptr;
     QNetworkAccessManager *m_network = nullptr;
+
+    QTemporaryDir *m_tempDir = nullptr;
+    QFile *m_downloadFile = nullptr;
+    QProgressDialog *m_progress = nullptr;
 };

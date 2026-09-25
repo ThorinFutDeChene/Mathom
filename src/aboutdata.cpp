c3225e157519ac3cb9af6823b434e4ccd23ad9fe
Thorin_01 <fabrice.pereyron@proton.me>
2026-09-25 23:34:05 +0200

    Mark help cleanup build as 0.1.9-dev1

--- src/aboutdata.cpp
+++ src/aboutdata.cpp
@@ -14,7 +14,7 @@
 AboutData::AboutData()
     : KAboutData(AboutData::componentName(),
                  AboutData::displayName(),
-                 QStringLiteral("0.1.8-stable"),
+                 QStringLiteral("0.1.9-dev1"),
                  i18n("<p><b>Taking care of your ideas.</b></p>"
                       "<p>A note-taking application that makes it easy to record ideas as you think, and quickly find them later. "
                       "Organizing your notes has never been so easy.</p>"),

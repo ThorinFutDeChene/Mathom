23d519e6c84f099e6bee7ef5d8d2cd0ede50edd1
Thorin_01 <fabrice.pereyron@proton.me>
2026-09-26 17:39:49 +0200

    Remove obsolete bundled Mathom PNG icons

--- src/archive.cpp
+++ src/archive.cpp
@@ -566,7 +566,7 @@ Archive::createArchiveFromSource(const QString &sourcePath, const QString &previ
     /// \todo write a way to create preview the way it's done in Archive::save
     QString previewImagePath = previewImage;
     if (previewImage.isEmpty() && !QFileInfo(previewImage).exists()) {
-        previewImagePath = QStringLiteral(":/images/128-apps-fr.thorinux.mathom.png");
+        previewImagePath = QStringLiteral(":/mathom/icons/app.png");
     }
 
     // Finally Save to the Real Destination file:
